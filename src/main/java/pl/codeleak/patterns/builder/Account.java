ea80ba14d09e38938e914b386965f518a45c53f1
monmar1104 <marcinpastwa@wp.pl>
2018-02-25 19:20:39 +0100

    Builder

--- src/main/java/pl/codeleak/patterns/builder/Account.java
+++ src/main/java/pl/codeleak/patterns/builder/Account.java
@@ -11,9 +11,17 @@ class Account {
     private LocalDateTime expiresAt; // optional
 
 
+
     Account(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    public Account(Builder builder) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.expiresAt = expiresAt;
     }
 
     String getName() {
@@ -38,5 +46,44 @@ class Account {
 
     public void setExpiresAt(LocalDateTime expiresAt) {
         this.expiresAt = expiresAt;
+    }
+
+    static class Builder{
+        private final String email;
+        private final String password;
+        private String name;
+        private LocalDateTime expiresAt;
+
+        public static Builder getBuilder(String email, String password){
+            return new Builder(email,password);
+        }
+
+        Builder(String email, String password){
+            this.email=email;
+            this.password=password;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public LocalDateTime getExpiresAt() {
+            return expiresAt;
+        }
+
+        public Builder setExpiresAt(LocalDateTime expiresAt) {
+            this.expiresAt = expiresAt;
+            return this;
+        }
+
+        public Account build(){
+            return new Account(this);
+        }
+
     }
 }
